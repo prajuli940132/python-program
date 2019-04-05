c3711b23699914d7cab3cc42079fa5fb1db18737
prakash parajuli <35681413+prajuli940132@users.noreply.github.com>
2019-04-05 17:25:31 +0530

    Add files via upload

---
+++ yolo/yolo.py
@@ -0,0 +1,299 @@
+#vehicle detection/trackng using yolo,openCV,dlib 
+#How to : python yolo.py --input videos/airport.mp4 --output output/airport_output.avi --yolo yolo-coco
+
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+import cv2
+import dlib
+import time
+import threading
+import math
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,help="path to input video")
+ap.add_argument("-o", "--output", required=True,help="path to output video")
+ap.add_argument("-y", "--yolo", required=True,help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+	help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())
+# os.remove(args["output"])
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+	dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
+configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+vs = cv2.VideoCapture(args["input"])
+writer = None
+(W, H) = (None, None)
+
+# try to determine the total number of frames in the video file
+try:
+	prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+		else cv2.CAP_PROP_FRAME_COUNT
+	total = int(vs.get(prop))
+	print("[INFO] {} total frames in video".format(total))
+
+# an error occurred while trying to determine the total
+# number of frames in the video file
+except:
+	print("[INFO] could not determine # of frames in video")
+	print("[INFO] no approx. completion time can be provided")
+	total = -1
+
+# loop over frames from the video file stream
+while True:
+	# read the next frame from the file
+	(grabbed, frame) = vs.read()
+
+	# if the frame was not grabbed, then we have reached the end
+	# of the stream
+	if not grabbed:
+		break
+
+	# if the frame dimensions are empty, grab them
+	if W is None or H is None:
+		(H, W) = frame.shape[:2]
+
+	# construct a blob from the input frame and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes
+	# and associated probabilities
+	blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+		swapRB=True, crop=False)
+	net.setInput(blob)
+	start = time.time()
+	layerOutputs = net.forward(ln)
+	end = time.time()
+
+	# initialize our lists of detected bounding boxes, confidences,
+	# and class IDs, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
+
+	# loop over each of the layer outputs
+	for output in layerOutputs:
+		# loop over each of the detections
+		for detection in output:
+			# extract the class ID and confidence (i.e., probability)
+			# of the current object detection
+			scores = detection[5:]
+			classID = np.argmax(scores)
+			confidence = scores[classID]
+
+			# filter out weak predictions by ensuring the detected
+			# probability is greater than the minimum probability
+			if confidence > args["confidence"]:
+				# scale the bounding box coordinates back relative to
+				# the size of the image, keeping in mind that YOLO
+				# actually returns the center (x, y)-coordinates of
+				# the bounding box followed by the boxes' width and
+				# height
+				box = detection[0:4] * np.array([W, H, W, H])
+				(centerX, centerY, width, height) = box.astype("int")
+
+				# use the center (x, y)-coordinates to derive the top
+				# and and left corner of the bounding box
+				x = int(centerX - (width / 2))
+				y = int(centerY - (height / 2))
+
+				# update our list of bounding box coordinates,
+				# confidences, and class IDs
+				boxes.append([x, y, int(width), int(height)])
+				confidences.append(float(confidence))
+				classIDs.append(classID)
+
+	# apply non-maxima suppression to suppress weak, overlapping
+	# bounding boxes
+	idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
+		args["threshold"])
+
+	# ensure at least one detection exists
+	if len(idxs) > 0:
+		# loop over the indexes we are keeping
+		for i in idxs.flatten():
+			# extract the bounding box coordinates
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+
+			# draw a bounding box rectangle and label on the frame
+			color = [int(c) for c in COLORS[classIDs[i]]]
+			cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+			text = "{}".format(LABELS[classIDs[i]]) #,confidences[i]
+			cv2.putText(frame, text, (x, y - 5),cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+	# check if the video writer is None
+	if writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 30,(frame.shape[1], frame.shape[0]), True)
+
+		# some information on processing single frame
+		if total > 0:
+			elap = (end - start)
+			print("[INFO] single frame took {:.4f} seconds".format(elap))
+			print("[INFO] estimated total time to finish: {:.4f}".format(elap * total))
+	# write the output frame to disk
+	writer.write(frame)
+
+# release the file pointers
+print("[INFO] cleaning up...")
+writer.release()
+vs.release()
+carCascade = cv2.CascadeClassifier('myhaar.xml')
+video = cv2.VideoCapture(args["output"])
+
+WIDTH = 1280
+HEIGHT = 720
+
+
+def estimateSpeed(location1, location2):
+	d_pixels = math.sqrt(math.pow(location2[0] - location1[0], 2) + math.pow(location2[1] - location1[1], 2))
+	# ppm = location2[2] / carWidht
+	ppm = 8.8
+	d_meters = d_pixels / ppm
+	#print("d_pixels=" + str(d_pixels), "d_meters=" + str(d_meters))
+	fps = 18
+	speed = d_meters * fps * 3.6
+	return speed
+	
+
+def trackMultipleObjects():
+	rectangleColor = (0, 255, 0)
+	frameCounter = 0
+	currentCarID = 0
+	fps = 0
+	
+	carTracker = {}
+	carNumbers = {}
+	carLocation1 = {}
+	carLocation2 = {}
+	speed = [None] * 1000
+	
+	# Write output to video file
+	out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (WIDTH,HEIGHT))
+
+	while True:
+		start_time = time.time()
+		rc, image = video.read()
+		if type(image) == type(None):
+			break
+		
+		image = cv2.resize(image, (WIDTH, HEIGHT))
+		resultImage = image.copy()
+		
+		frameCounter = frameCounter + 1
+		
+		carIDtoDelete = []
+
+		for carID in carTracker.keys():
+			trackingQuality = carTracker[carID].update(image)
+			
+			if trackingQuality < 7:
+				carIDtoDelete.append(carID)
+				
+		for carID in carIDtoDelete:
+			print ('Removing carID ' + str(carID) + ' from list of trackers.')
+			print ('Removing carID ' + str(carID) + ' previous location.')
+			print ('Removing carID ' + str(carID) + ' current location.')
+			carTracker.pop(carID, None)
+			carLocation1.pop(carID, None)
+			carLocation2.pop(carID, None)
+		
+		if not (frameCounter % 10):
+			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+			cars = carCascade.detectMultiScale(gray, 1.1, 13, 18, (24, 24))
+			
+			for (_x, _y, _w, _h) in cars:
+				x = int(_x)
+				y = int(_y)
+				w = int(_w)
+				h = int(_h)
+			
+				x_bar = x + 0.5 * w
+				y_bar = y + 0.5 * h
+				
+				matchCarID = None
+			
+				for carID in carTracker.keys():
+					trackedPosition = carTracker[carID].get_position()
+					
+					t_x = int(trackedPosition.left())
+					t_y = int(trackedPosition.top())
+					t_w = int(trackedPosition.width())
+					t_h = int(trackedPosition.height())
+					
+					t_x_bar = t_x + 0.5 * t_w
+					t_y_bar = t_y + 0.5 * t_h
+				
+					if ((t_x <= x_bar <= (t_x + t_w)) and (t_y <= y_bar <= (t_y + t_h)) and (x <= t_x_bar <= (x + w)) and (y <= t_y_bar <= (y + h))):
+						matchCarID = carID
+				
+				if matchCarID is None:
+					print ('Creating new tracker ' + str(currentCarID))
+					
+					tracker = dlib.correlation_tracker()
+					tracker.start_track(image, dlib.rectangle(x, y, x + w, y + h))
+					
+					carTracker[currentCarID] = tracker
+					carLocation1[currentCarID] = [x, y, w, h]
+
+					currentCarID = currentCarID + 1
+
+		for carID in carTracker.keys():
+			trackedPosition = carTracker[carID].get_position()		
+			t_x = int(trackedPosition.left())
+			t_y = int(trackedPosition.top())
+			t_w = int(trackedPosition.width())
+			t_h = int(trackedPosition.height())
+			carLocation2[carID] = [t_x, t_y, t_w, t_h]
+		
+		end_time = time.time()
+		
+		if not (end_time == start_time):
+			fps = 1.0/(end_time - start_time)
+		for i in carLocation1.keys():	
+			if frameCounter % 1 == 0:
+				[x1, y1, w1, h1] = carLocation1[i]
+				[x2, y2, w2, h2] = carLocation2[i]
+		
+				# print 'previous location: ' + str(carLocation1[i]) + ', current location: ' + str(carLocation2[i])
+				carLocation1[i] = [x2, y2, w2, h2]
+		
+				# print 'new previous location: ' + str(carLocation1[i])
+				if [x1, y1, w1, h1] != [x2, y2, w2, h2]:
+					if (speed[i] == None or speed[i] == 0) and y1 >= 275 and y1 <= 285:
+						speed[i] = estimateSpeed([x1, y1, w1, h1], [x2, y2, w2, h2])
+
+					#if y1 > 275 and y1 < 285:
+					if speed[i] != None and y1 >= 180:
+						cv2.putText(resultImage, str(int(speed[i])) + " km/hr", (int(x1 + w1/2), int(y1-5)),cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
+		cv2.imshow('result', resultImage)
+		# Write the frame into the file 'output.avi'
+		if cv2.waitKey(33) == 27:
+			break
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	trackMultipleObjects()
